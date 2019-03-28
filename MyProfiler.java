41ba2234bdbf56f75c13f9028c1bf9d38b9de310
rkashpar <47769570+rkashpar@users.noreply.github.com>
2019-03-28 14:17:25 -0500

    Add files via upload

--- MyProfiler.java
+++ MyProfiler.java
@@ -1,12 +1,12 @@
 /**
  * Filename:   MyProfiler.java
  * Project:    p3b-201901     
- * Authors:    TODO: add your name(s) and lecture numbers here
+ * Authors:    Roma Kashpar, lecture 001
  *
  * Semester:   Spring 2019
  * Course:     CS400
  * 
- * Due Date:   TODO: add assignment due date and time
+ * Due Date:   3-28-2019 10pm
  * Version:    1.0
  * 
  * Credits:    TODO: name individuals and sources outside of course staff
@@ -15,6 +15,7 @@
  */
 
 // Used as the data structure to test our hash table against
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class MyProfiler<K extends Comparable<K>, V> {
@@ -23,32 +24,54 @@ public class MyProfiler<K extends Comparable<K>, V> {
     TreeMap<K, V> treemap;
     
     public MyProfiler() {
-        // TODO: complete the Profile constructor
         // Instantiate your HashTable and Java's TreeMap
+    	
+    	hashtable = new HashTable<K, V>();
+    	treemap = new TreeMap<K, V>();
     }
     
     public void insert(K key, V value) {
-        // TODO: complete insert method
         // Insert K, V into both data structures
+    	try {
+    		hashtable.insert(key, value);
+    		treemap.put(key, value);
+    	}
+    	catch (IllegalNullKeyException e) {}
+    	catch (DuplicateKeyException f) {}
     }
     
     public void retrieve(K key) {
-        // TODO: complete the retrieve method
         // get value V for key K from data structures
+    	
+    	try {
+    		hashtable.get(key);
+    		treemap.get(key);
+    	}
+    	catch (KeyNotFoundException g) {}
+    	catch (IllegalNullKeyException h) {}
     }
     
     public static void main(String[] args) {
         try {
             int numElements = Integer.parseInt(args[0]);
+            MyProfiler<Integer, Integer> profile = new MyProfiler<Integer, Integer>();
 
+            ArrayList<Integer> elements = new ArrayList<Integer>();
             
-            // TODO: complete the main method. 
+            for (int i = 0; i < numElements; i++) {
+                profile.insert(i, i);
+                
+            }
+            
+            for (int j = 0; j < numElements; j++) {
+            	profile.retrieve(j);
+            }
+            
             // Create a profile object. 
             // For example, Profile<Integer, Integer> profile = new Profile<Integer, Integer>();
             // execute the insert method of profile as many times as numElements
             // execute the retrieve method of profile as many times as numElements
-            // See, ProfileSample.java for example.
-            
+            // See, ProfileSample.java for example.           
         
             String msg = String.format("Inserted and retreived %d (key,value) pairs", numElements);
             System.out.println(msg);
